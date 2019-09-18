eaab468c1457d8707fc70a11a4793f1c240b9e71
Logan Powell <loganp@tepper.cmu.edu>
2019-09-18 09:26:13 -0400

    terser

--- hdom/index.js
+++ hdom/index.js
@@ -3,19 +3,12 @@ import { start, renderOnce } from '@thi.ng/hdom'
 // stateless component w/ params
 // the first arg is an auto-injected context object
 // (not used here, see dedicated section in readme further below)
+// Emmet-style tags with ID and/or classes are supported.
 const greeter = (_, name) => ['h1.title', 'hello ', name]
-
-// component w/ local state
-const counter = (i = 0) => {
-  return () => ['button', { onclick: () => i++ }, `clicks: ${i}`]
-}
-
-const app = () => {
-  // initialization steps
-  // ...
-  // root component is just a static array
-  return ['div#app', [greeter, 'world'], counter(), counter(100)]
-}
+// component w/ local state via H.O.F.
+const counter = (i = 0) => () => ['button', { onclick: () => i++ }, `clicks: ${i}`]
+// root component is just a static array
+const app = () => ['div#app', [greeter, 'world'], counter(), counter(100)]
 
 // start RAF update & diff loop
 start(app(), { root: document.body })
